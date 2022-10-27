b7cc17c08781600a6af24ffdc862e1294fc4337d
Haastrup88 <haastrupbay012@gmail.com>
2022-10-27 19:45:04 +0100

    commit

--- py.py
+++ py.py
@@ -1,4 +1,6 @@
 import random
+
+
 print("Welcome to GAME OF CRAPS text_based game")
 print(" ")
 print("*****Game Instructions****")
@@ -8,3 +10,29 @@ player1=input("Player One! Kindly provide your name:")
 player2=input("Player Two! Kindly provide your name:")
 
 print(f"Welcome! {player1} and {player2}")
+
+
+
+def shooters():
+    player1_score=0
+    player2_score=0
+    round=1
+    while round!=3:
+        player=input(f"First shooter? {player1} or {player2}:")
+        if player.lower().strip()==player1:
+            shooter=player1
+        elif(player.lower().strip()==player2):
+            shooter=player2
+        else:
+            print("Invalid Input")
+        round=round+1
+        return shooter
+
+
+def dice_roll():
+    dice_1=random.randint(1,6)
+    dice_2=random.randint(1,6)
+    result=(dice_1,dice_2)
+    return(result)
+
+shooters()
