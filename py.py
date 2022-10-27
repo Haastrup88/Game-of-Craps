2ceef5722328d7a5e445c8080c5c33cca4bff240
Haastrup88 <haastrupbay012@gmail.com>
2022-10-27 18:03:34 +0100

    first commit

---
+++ py.py
@@ -0,0 +1,10 @@
+import random
+print("Welcome to GAME OF CRAPS text_based game")
+print(" ")
+print("*****Game Instructions****")
+print("This involve two players, and both players have to provide thier names")
+print(" ")
+player1=input("Player One! Kindly provide your name:")
+player2=input("Player Two! Kindly provide your name:")
+
+print(f"Welcome! {player1} and {player2}")
