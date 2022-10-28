7403b6807e57e6f7d1445e6a3eba641c148ab4d4
Haastrup88 <haastrupbay012@gmail.com>
2022-10-28 20:28:16 +0100

    first

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from logging import exception
 import random
 
 class Dice(object):
@@ -7,5 +8,35 @@ class Dice(object):
     def roll(self):
         return(f"Outcome:{self.dice1},{self.dice2}")
 
-result=Dice(random.randint(1,6),random.randint(1,6))
-print(result.roll())
+class Table(Dice):
+    def __init__(self,dice1,dice2,point):
+        super().__init__(dice1,dice2)
+        self.point=point
+    def point(self):
+        return(f"Point:{self.point}")
+
+class Player(Table):
+    def __init__(self,dice1,dice2,point,name):
+        super().__init__(dice1,dice2,point)
+        self.name=name
+
+    def bankroll(self):
+        try:
+          amount=input(f" {self.name}, what is your bankroll?")
+        except:
+            print("Bankroll must be more than 10 dollars")
+            amount=0
+        return(amount) 
+        
+
+    def Name(self):
+        print(f"Player name is {self.name}")
+
+
+print("***This is GAME OF CRAPS***")
+print("**Kindly respond to the questions below;")
+player=input("Enter your name:")
+print(f"Welcome {player}")
+
+result=Player(random.randint(1,6),random.randint(1,6),"Off",player)
+print(result.bankroll())
