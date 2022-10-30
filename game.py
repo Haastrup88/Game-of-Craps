e777f2720ed533d6be47d8487ce8419de3b0babf
Haastrup88 <haastrupbay012@gmail.com>
2022-10-30 22:21:30 +0100

    commit

--- game.py
+++ game.py
@@ -1,25 +1,29 @@
 import Bet 
-import Player
-import Table
+import time
 
 def come_out_phase(result):
-    #print(result.update_details())
     print(f"Game type:Craping")
     if result.player_status==result.option[0]:
         result.bankroll=result.bankroll-result.bet
-        print(result.bankroll)
+        time.sleep(2.5)
         print(result.update_details())
+        time.sleep(2.5)
         update(result)
     elif((result.player_status==result.option[1])):
-        result.bankroll=result.bankroll+result.bet 
-        #print(result.bankroll)
+        result.bankroll=result.bankroll+result.bet
+        time.sleep(2.5) 
         print(result.update_details())
     if (result.dice_result==7 or result.dice_result==11):
         if result.player_status==result.option[0]:
             result.bankroll=result.bankroll+result.bet
+            time.sleep(2.5) 
+            print(result.update_details())
         else:
-            if result.player_status==result.option[0]:
-                result.bankroll=result.bankroll+result.bet
+            if result.player_status==result.option[1]:
+                result.bankroll=result.bankroll-result.bet
+                time.sleep(2.5) 
+                print(result.update_details())
+
     else:
         pass
     if (result.dice_result) in points:
@@ -31,14 +35,17 @@ def come_out_phase(result):
 def point_phase(result):
     print(result.update_details())
     #print(result.update_bankroll(result.bet))
+    time.sleep(2.5) 
     update(result)
     #print(result.info())
     if (result.dice_result) in points:
         if result.player_status==result.option[0]:
             result.bankroll=result.bankroll+result.bet
+            time.sleep(2.5) 
             print(result.update_details())
         elif(result.player_status==result.option[1]):
             result.bankroll=result.bankroll-result.bet
+            time.sleep(2.5) 
             print(result.update_details())
         else:
             pass
@@ -48,6 +55,7 @@ def point_phase(result):
             print(result.update_details())
         elif(result.player_status==result.option[1]):
             result.bankroll=result.bankroll+result.bet
+            time.sleep(2.5) 
             print(result.update_details())
     return(result.info())
 
@@ -81,6 +89,7 @@ def update(result):
             come_out_phase(result)
         elif outcome in points:
             point_phase(result)
+            point=point+1
         else:
             pass
         round=round+1
