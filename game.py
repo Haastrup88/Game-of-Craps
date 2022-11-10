9281780ea2d2bcbd45893378e7e19e3e80b7c994
Haastrup88 <haastrupbay012@gmail.com>
2022-11-10 20:07:43 +0100

    commit

--- game.py
+++ game.py
@@ -107,6 +107,7 @@ def pass_bet(result):# Passline function at point phase
 def point_bet(result):
     odd_bet=int(input("How much do you want to bet? "))
     while(odd_bet>max_bet or odd_bet>result.bankroll):
+        
         try:
             odd_bet=int(input(f"Your point wager must be lesser than ${max_bet}, likewise your bankroll of ${result.bankroll}.How much do you want to bet?"))        
                         
